410009b40cfc8897f49bac96ef2f167ca3f4fdf9
runjief <90967244+runjief@users.noreply.github.com>
2024-09-06 16:09:28 +0000

    feat: filter video details and migrate blocked users

--- feed-filter-script.ts
+++ feed-filter-script.ts
@@ -2,52 +2,52 @@
 // @namespace urn:runjief:feed-filter-script
 // @name feed-filter-script
 // @description 避免看到指定用户上传的视频，在用户个人主页会多出一个屏蔽按钮。
-// @version  1
 // @grant    GM.getValue
 // @grant    GM.setValue
+// @grant    GM.deleteValue
 // @include	 https://search.bilibili.com/*
 // @include	 https://space.bilibili.com/*
 // @include	 https://www.bilibili.com/*
 // @run-at   document-idle
 // ==/UserScript==
 
-// spell-checker: word bili bilibili
+// spell-checker: word bili bilibili upname
 
 import obtainHTMLElement from "./utils/obtainHTMLElement";
-import toggleArrayItem from "./utils/toggleArrayItem";
 import useGMValue from "./utils/useGMValue";
+import usePolling from "./utils/usePolling";
 
 export {};
 
-const blockedUserIDs = useGMValue(
-  "blockedUserIDs@89f186cd-f6ba-530d-b8b2-171f916d8888",
-  [] as string[]
+const blockedUsers = useGMValue(
+  "blockedUsers@b020a2c5-0839-5573-9fcf-7eba106ff8c4",
+  {} as Record<string, boolean | undefined>
 );
 
-function renderBlockButton(userID: string) {
-  const { isLoading } = blockedUserIDs;
-
-  const isBlocked = blockedUserIDs.value.includes(userID);
-
-  const el = obtainHTMLElement(
-    "button",
-    "89f186cd-f6ba-530d-b8b2-171f916d8888"
-  );
-  el.setAttribute("type", "button");
-  el.classList.add("h-f-btn");
-  el.style.width = "auto";
-  el.style.minWidth = "76px";
-  el.textContent = isBlocked ? "取消屏蔽" : "屏蔽";
-  if (isLoading) {
-    el.textContent = "加载屏蔽列表...";
+async function migrateV1() {
+  const key = "blockedUserIDs@89f186cd-f6ba-530d-b8b2-171f916d8888";
+  const oldValue = await GM.getValue(key);
+  if (!oldValue) {
+    return;
   }
-  el.onclick = () => {
-    if (isLoading) {
-      return;
-    }
-    const arr = blockedUserIDs.value.slice();
-    toggleArrayItem(arr, userID);
-    blockedUserIDs.value = arr;
+  const newValue = { ...blockedUsers.value };
+  (JSON.parse(String(oldValue)) as string[]).forEach((i) => {
+    newValue[i] = true;
+  });
+  blockedUsers.value = newValue;
+  await GM.deleteValue(key);
+}
+
+function renderBlockButton(userID: string) {
+  const isBlocked = blockedUsers.value[userID];
+  const el = obtainHTMLElement("span", "89f186cd-f6ba-530d-b8b2-171f916d8888");
+  el.classList.add("h-f-btn");
+  el.textContent = isBlocked ? "取消屏蔽" : "屏蔽";
+  el.onclick = async () => {
+    blockedUsers.value = {
+      ...blockedUsers.value,
+      [userID]: !isBlocked || undefined,
+    };
     renderBlockButton(userID);
   };
 
@@ -55,41 +55,74 @@ function renderBlockButton(userID: string) {
   parent.prepend(el);
 }
 
+function parseUserURL(rawURL: string | undefined): string | undefined {
+  if (!rawURL) {
+    return;
+  }
+  const url = new URL(rawURL, window.location.href);
+  if (url.host !== "space.bilibili.com") {
+    return;
+  }
+  const match = /^\/(\d+)\/?/.exec(url.pathname);
+  if (!match) {
+    return;
+  }
+  return match[1];
+}
+
 function renderVideoCard() {
   document.querySelectorAll(".bili-video-card").forEach((i) => {
     const rawURL = i
       .querySelector("a.bili-video-card__info--owner")
       ?.getAttribute("href");
+    const userID = parseUserURL(rawURL);
+    if (!userID) {
+      return;
+    }
+    const isBlocked = blockedUsers.value[userID];
+    const container = i.parentElement.classList.contains("video-list-item")
+      ? i.parentElement
+      : i;
+    if (isBlocked) {
+      container.setAttribute("hidden", "");
+    } else {
+      container.removeAttribute("hidden");
+    }
+  });
+
+  document.querySelectorAll(".video-page-card-small").forEach((i) => {
+    const rawURL = i.querySelector(".upname a")?.getAttribute("href");
     if (!rawURL) {
       return;
     }
-    const match = /^\/(\d+)$/.exec(
-      new URL(rawURL, window.location.href).pathname
-    );
-    if (!match) {
+    const userID = parseUserURL(rawURL);
+    if (!userID) {
       return;
     }
-    const userID = match[1];
-    const isBlocked = blockedUserIDs.value.includes(userID);
+    const isBlocked = blockedUsers.value[userID];
+    const container = i;
     if (isBlocked) {
-      i.setAttribute("hidden", "");
+      container.setAttribute("hidden", "");
     } else {
-      i.removeAttribute("hidden");
+      container.removeAttribute("hidden");
     }
   });
 }
 
-function main() {
+async function main() {
+  await migrateV1();
   if (window.location.host === "space.bilibili.com") {
-    const match = /^\/(\d+)$/.exec(window.location.pathname);
-    if (!match) {
+    const userID = parseUserURL(window.location.href);
+    if (!userID) {
       return;
     }
-    const userID = match[1];
-    renderBlockButton(userID);
-    setInterval(() => renderBlockButton(userID), 200);
+    usePolling({
+      update: () => renderBlockButton(userID),
+    });
   } else {
-    setInterval(() => renderVideoCard(), 200);
+    usePolling({
+      update: () => renderVideoCard(),
+    });
   }
 }
 
